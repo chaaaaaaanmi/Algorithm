658148d05a6352afdfb1312a98a116f19bf507dc
chaaaaaaanmi <chanmii1205@gmail.com>
2025-06-17 22:49:21 +0900

    [Bronze V] Title: 윤년, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,12 +1,13 @@
 def solve(n):
     
-    if n % 4 == 0:
-        if n % 100 != 0:
-            return 1
-        if n % 400 == 0:
-            return 1
+    if n % 4 == 0 and n % 100 != 0:
+        return 1
+    
+    if n % 400 == 0:
+        return 1
 
     return 0
+
 
 a = int(input())
 print(solve(a))
