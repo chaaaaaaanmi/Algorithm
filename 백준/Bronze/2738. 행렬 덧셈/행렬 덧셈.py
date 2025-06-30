4b8f82c65ac078a8761339019c03b7a45df6e88d
chaaaaaaanmi <chanmii1205@gmail.com>
2025-06-30 23:15:24 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(n)]
+B = [list(map(int, input().split())) for _ in range(n)]
+result = [[0] * m for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        result[i][j] = A[i][j] + B[i][j]
+
+for i in range(n):
+    for j in range(m):
+        print(result[i][j], end=" ")
+    print()
