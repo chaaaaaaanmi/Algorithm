b834f75ccc4af30d228dad988da701e547e3d8ac
chaaaaaaanmi <chanmii1205@gmail.com>
2025-08-07 21:31:24 +0900

    [level 2] Title: 카펫, Time: 3277.77 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        
+        int total = brown + yellow;
+        
+        // 가로, 세로 최소 길이는 3
+        for (int r = 3; r <= total; r++) {
+            for (int c = 3; c <= total; c++) {
+                if ((r-2)*(c-2) == yellow &&
+                   (r*2)+(2*(c-2)) == brown) {
+                    return new int[]{c,r};
+
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
