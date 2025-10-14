45eb1b385972c9fae31f138e9e181767dc712c0b
chaaaaaaanmi <chanmii1205@gmail.com>
2025-10-14 17:29:33 +0900

    [level 2] Title: 문자열 압축, Time: 3.62 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -2,42 +2,42 @@ def solution(s):
     
     if len(s) == 1:
         return 1
-           
-    answer = len(s) # 압축이 안 되는 경우
     
-    # cut 단위로 자르기
-    for cut in range(1, len(s)//2+1):
+    min_answer = len(s)
+    
+    # 자르는 단위는 1 ~ len(s)//2
+    for i in range(1, len(s)//2+1):
         
-        prev = s[:cut]
-        count = 1
-        result = ""
+        answer = 0
+        cnt = 1 # 문자열 반복 횟수 (일단 하나는 존재하는거니까 1)
+        string = s[:i] # 비교할 문자열
         
-        # cut 단위로 반복
-        for i in range(cut, len(s), cut):
-            current = s[i:i+cut]
-            
-            # 중복이면 count + 1
-            if current == prev:
-                count += 1
-                
-            # 그게 아니면 result에 붙이기
+        # i 단위로 자르면서 비교해보기
+        for j in range(i, len(s), i):
+            # 현재 문자열
+            current = s[j:j+i]
+        
+            # 비교할 문자열과 현재 문자열이 같다
+            if string == current:
+                cnt += 1
+                continue
+
+            # 비교할 문자열과 현재 문자열이 다르다
             else:
-                if count > 1:
-                    result += str(count) + prev
-                else:
-                    result += prev
-                    
-                # 다음 비교할 문자열로 갱신
-                prev = current
-                count = 1
-        
-        # 마지막 덩어리 처리
-        if count > 1:
-            result += str(count) + prev
-        else:
-            result += prev
-            
-        # 가장 짧은 길이 갱신
-        answer = min(answer, len(result))
-        
-    return answer
+                answer += len(string)
+                # cnt가 1이면 생략
+                if cnt > 1:
+                    answer += len(str(cnt))
+                
+                string = current # 비교할 문자열 갱신
+                cnt = 1 # cnt 초기화
+                
+        # 마지막 그룹까지 계산
+        answer += len(string)
+        if cnt > 1:
+            answer += len(str(cnt))
+                
+        # min 갱신
+        min_answer = min(answer, min_answer)
+    
+    return min_answer
