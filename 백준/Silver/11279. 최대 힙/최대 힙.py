87b3c930c69fff5310c802c990ae16c8fb73e7e7
chaaaaaaanmi <chanmii1205@gmail.com>
2025-11-24 16:48:25 +0900

    [Silver II] Title: 최대 힙, Time: 108 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,18 @@
+import heapq, sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+
+for _ in range(n):
+
+    x = int(input())
+
+    if x == 0:
+        if arr:
+            print(-heapq.heappop(arr))
+        else:
+            print(0)
+
+    else:
+        heapq.heappush(arr, -x)
