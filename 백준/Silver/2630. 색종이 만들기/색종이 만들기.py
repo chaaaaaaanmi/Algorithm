b8cf9fd81c09177bf548b14b95249f5f76e6610e
chaaaaaaanmi <chanmii1205@gmail.com>
2025-11-24 17:49:37 +0900

    [Silver II] Title: 색종이 만들기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+paper = [list(map(int, input().split())) for _ in range(n)]
+
+white = 0
+blue = 0
+
+def solve(x, y, size):
+    global white, blue
+
+    color = paper[x][y]
+
+    for i in range(x, x+size):
+        for j in range(y, y+size):
+            # size로 나눈 기준 (x,y) 와 색이 다르면 size//2
+            if paper[i][j] != color:
+                half = size // 2
+                solve(x, y, half)
+                solve(x+half, y, half)
+                solve(x, y+half, half)
+                solve(x+half, y+half, half)
+                return
+            
+    if color == 0:
+        white += 1
+    else:
+        blue += 1
+
+solve(0, 0, n)
+print(white)
+print(blue)
