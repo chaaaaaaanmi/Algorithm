0f5364b330933b593bde0346f8039c65e87a2e2a
chaaaaaaanmi <chanmii1205@gmail.com>
2025-11-26 21:36:19 +0900

    [Silver II] Title: 마인크래프트, Time: 792 ms, Memory: 112916 KB -BaekjoonHub

---
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -0,0 +1,28 @@
+# n: 세로, m: 가로, b: 인벤토리 내 블록 개수
+n, m, b = map(int, input().split())
+MAP = [list(map(int, input().split())) for _ in range(n)]
+
+time = 500 * 500 * 256 * 2
+height = 0
+for h in range(0, 257):
+    need = 0
+    remove = 0
+
+    for x in range(n):
+        for y in range(m):
+            if MAP[x][y] < h:
+                need += h - MAP[x][y]
+            elif MAP[x][y] > h:
+                remove += MAP[x][y] - h
+
+    # 인벤토리에 있는 블록보다 더 많은 블록이 필요하면 이 높이는 불가능
+    if need - remove > b:
+        continue
+
+    current_time = (need * 1) + (remove * 2)
+    if current_time <= time:
+        time = current_time
+        height = h
+
+
+print(time, height, sep=" ")
