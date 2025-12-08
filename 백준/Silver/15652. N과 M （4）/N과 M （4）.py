ab0944a34c447d79e195ae4c31afa70afc7c803f
chaaaaaaanmi <chanmii1205@gmail.com>
2025-12-08 15:28:10 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,15 @@
+def solve(start):
+
+    if len(answer) == m:
+        print(*answer)
+        return
+    
+    for i in range(start, n+1):
+        answer.append(i)
+        solve(i)
+        answer.pop() # 복구
+
+
+n, m = map(int, input().split())
+answer = []
+solve(1)
